7e878f9dff90975977c6b66bfdb67997ee15d421
Jivenlans Tabien <Jivenlans Tabien>
2017-10-05 09:09:57 +0800

    Add project files.

---
+++ CoolBreeze/CoolBreeze.Android/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace CoolBreeze.Droid
+{
+    [Activity(Label = "CoolBreeze", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            Microsoft.Azure.Mobile.MobileCenter.Configure("1d7d28be-b4a8-4e3d-bdde-580fe59625a5");
+            LoadApplication(new App());
+        }
+    }
+}
+
---
+++ CoolBreeze/CoolBreeze/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace CoolBreeze
+{
+    public partial class App : Application
+    {
+        public static MainViewModel ViewModel;
+        public static string RegistrationCode = "XBWW63199CCA79B09F4382D613C67AA2";
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new CoolBreeze.StartPage();
+        }
+
+        protected override void OnStart()
+        {
+            MobileCenter.Start(typeof(Analytics), typeof(Crashes));
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
